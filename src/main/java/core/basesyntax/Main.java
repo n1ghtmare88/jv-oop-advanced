944ddfe4f9c542f3f59f42e9d2d9363e88d54f0f
Dmitriy Yevtushenko <dmitriy.zp.city@gmail.com>
2026-03-30 16:19:28 +0300

    fixed main method

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,16 +1,18 @@
 package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
+        final int SIZE = 6;
         FigureSupplier figureSupplier = new FigureSupplier();
-        Figure [] figures = new Figure[10];
+        Figure [] figures = new Figure[SIZE];
         for (int i = 0; i < figures.length; i++) {
             if (i < figures.length / 2) {
                 figures[i] = figureSupplier.getRandomFigure();
-                figures[i].draw();
             } else {
                 figures[i] = figureSupplier.getDefaultFigure();
-                figures[i].draw();
             }
+        }
+        for (Figure value : figures) {
+            value.draw();
         }
     }
 }
